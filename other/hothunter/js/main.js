db7a3e8182c0e032cdf91c307fdfea6e183f7e38
Ralf Wang <ebates@RalfdeMacBook-Pro.local>
2015-04-13 10:47:33 +0800

    git

---
+++ other/hothunter/js/main.js
@@ -0,0 +1,20 @@
+(function(window,$) {
+    
+    'use strict';
+
+    
+    //home view js
+    (function() {
+        
+        var $home = $('.home'),
+        screenHeight = document.documentElement.clientHeight || document.body.clientHeight || 0;
+        
+        
+        $home.height(screenHeight);
+        
+        
+    })();
+    
+    
+    
+})(window,jQuery);
