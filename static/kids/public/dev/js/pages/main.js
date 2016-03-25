d378231b25e390939ea35e500977fd291637caa4
ralf <kingarthas37@hotmail.com>
2016-03-25 17:17:38 +0800

    react

--- static/kids/public/dev/js/pages/main.js
+++
@@ -1,8 +0,0 @@
-require('../../../js/main');
-
-require('./index/page');
-require('./index/nav');
-require('./index/image-view');
-require('./index/head-line');
-require('./index/image-list');
-require('./index/brand-list');
